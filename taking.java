1612ea1a74d9412e67790a442ceb1395c0d80a0c
tamimdiu <tatamim482@gmail.com>
2023-09-02 21:32:54 +0600

    my second comment

---
+++ taking.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class taking{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter first nUmber : ");
+        int a =sc.nextInt();
+        System.out.println("Enter second element number : ");
+        int b =sc.nextInt();
+        int c=a+b;
+        System.out.println("sum is "+c);
+    }
+}
